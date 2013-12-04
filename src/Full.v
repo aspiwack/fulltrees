a6353773bef35778ab24106b50d6a51d231116b7
Arnaud Spiwack <arnaud@spiwack.net>
2013-12-04 17:03:59 +0100

    Building a full tree from a list.
    An ocaml and a Coq encoding of the invariants of the full tree reconstruction inside the types.

---
+++ src/Full.v
@@ -0,0 +1,197 @@
+Require Import Coq.Lists.List.
+Import ListNotations.
+
+Notation "⟨ T , x , y ⟩" := (existT T x y).
+
+(** Full trees have two constructor for leaves, as they can appear
+    either at the lower level or at the level just above. The former
+    builds a full tree of height 0, and the latter of height
+    1. Alternatively, we could replace the lower leaf by a singleton
+    constructor. *)
+
+Inductive FullTree (A:Type) : nat -> Type :=
+  | Leaf₀ : FullTree A 0
+  | Leaf₁ : FullTree A 1
+  | Node {n:nat} : FullTree A n -> A -> FullTree A n -> FullTree A (S n)
+.
+Arguments Leaf₀ {A}.
+Arguments Leaf₁ {A}.
+Arguments Node {A n} _ _ _.
+
+Module BinaryList.
+
+  (** [T A] is a variant of Okasaki's binary list where the first
+  element is always explicit. The reason for this type is that the
+  tail of [l] or the tail of its tail is structurally smaller than
+  [l]. Which we will make use of to reconstruct power-lists. From a
+  number system point of view, [T A] is an decorated version of the
+  positive natural numbers written in binary base with digits [1] and
+  [2]. *)
+  Inductive T (A:Type) : Type :=
+  | one (a:A)
+  | two (a b:A)
+  | tpo (a:A) (l:T (A*A))
+  | tpt (a b: A) (l:T (A*A))
+  .
+  Arguments one {A} a.
+  Arguments two {A} a b.
+  Arguments tpo {A} a l.
+  Arguments tpt {A} a b l.
+
+  Fixpoint cons {A} (a:A) (l:T A) : T A :=
+    match l with
+    | one b => two a b
+    | two b c => tpo a (one (b,c))
+    | tpo b l => tpt a b l
+    | tpt b c l => tpo a (cons (b,c) l)
+    end
+  .
+
+  Fixpoint twice {A} (l:T (A*A)) : T A :=
+    match l with
+    | one (a,b) => two a b
+    | two (a,b) cd => tpt a b (one cd)
+    | tpo (a,b) l => tpt a b (twice l)
+    | tpt (a,b) cd l => tpt a b (tpo cd l)
+    end
+  .
+
+  Fixpoint of_ne_list {A} (a:A) (l:list A) : T A :=
+    match l with
+    | [] => one a
+    | b::l => cons a (of_ne_list b l)
+    end
+  .
+
+End BinaryList.
+
+Module PowerList.
+
+  (** The type of power lists is enriched compared to the Ocaml
+      version: we keep the height as an argument. For the sake of
+      simplicity, power lists are defined recursively rather than
+      inductively. *)
+  Fixpoint T (A:Type) (n:nat) :=
+    match n with
+    | 0 => A
+    | S n' => (A * T (A*A) n')%type
+    end
+  .
+
+  (** Variant of [T] with implicit height. *)
+  Definition U (A:Type) := { n:nat & T A n }.
+  Definition one {A:Type} (a:A) : U A := ⟨ T A , 0 , a ⟩.
+  Definition tpo {A:Type} (a:A) (l:U (A*A)) : U A :=
+    let '(existT n  l) := l in
+    ⟨ T A , S n , (a,l) ⟩.
+
+  Fixpoint map {A B n} (f:A->B) : T A n -> T B n :=
+    match n with
+    | 0 => fun t => f t
+    | S n => fun t =>
+      let f' xy := let '(x,y) := xy in (f x,f y) in
+      let '(x,l) := t in
+      ( f x , map f' l )
+    end
+  .
+
+  Module BL := BinaryList.
+
+  Fixpoint of_blist_pairing_with_cast {A O E} (d:A->E*O) (f:A->O) (g:A->E) (l:BL.T A) : U (E*O) :=
+    match l with
+    | BL.one a => one (d a)
+    | BL.two a b => one (g a , f b)
+    | BL.tpo a l =>
+      let d' xy := let '(x,y) := xy in (d x , d y) in
+      let f' xy := let '(x,y) := xy in (g x , f y) in
+      tpo (d a) (of_blist_pairing_with_cast d' f' f' l)
+    | BL.tpt a b l =>
+      let d' xy := let '(x,y) := xy in (d x , d y) in
+      let f' xy := let '(x,y) := xy in (g x , f y) in
+      tpo (g a,f b) (of_blist_pairing_with_cast d' f' f' l)
+    end
+  .
+
+End PowerList.
+
+Module AlternatingPowerList.
+
+  Module PL := PowerList.
+  Module BL := BinaryList.
+
+  Definition T (Odd Even:Type) (n:nat) : Type :=
+    match n with
+    | 0 => Odd
+    | S n => (Odd * PL.T (Even*Odd) n)%type
+    end
+  .
+
+  (** Variant of [T] with implicit height. *)
+  Definition U (Odd Even:Type) := { n:nat & T Odd Even n }.
+  Definition one {Odd Even:Type} (a:Odd) : U Odd Even := ⟨ T Odd Even , 0 , a ⟩.
+  Definition tpo {Odd Even:Type} (a:Odd) (l:PL.U (Even*Odd)) : U Odd Even :=
+    let '(existT n  l) := l in
+    ⟨ T Odd Even , S n , (a,l) ⟩.
+
+  Definition of_binary_list {A Odd Even} (d:Odd) (f:A->Odd) (g:A->Even) (l:BL.T A) : U Odd Even :=
+    match l with
+    | BL.one a => one (f a)
+    | BL.two a b => tpo (f a) (PL.one (g b , d))
+    | BL.tpo a l =>
+      let d' x := ( g x , d ) in
+      tpo (f a) (PL.of_blist_pairing_with_cast d' f g (BL.twice l))
+    | BL.tpt a b l  =>
+      let d' x := ( g x , d ) in
+      tpo (f a) (PL.of_blist_pairing_with_cast d' f g (BL.tpo b l))
+    end
+  .
+
+  Definition of_list {A Odd Even} (d:Odd) (f:A->Odd) (g:A->Even) (l:list A) : U Odd Even :=
+    match l with
+    | [] => one d
+    | a::l =>
+      of_binary_list d f g (BL.of_ne_list a l)
+    end
+  .
+
+End AlternatingPowerList.
+
+Module BL := BinaryList.
+Module PL := PowerList.
+Module APL := AlternatingPowerList.
+
+Definition pass {A n p} : APL.T (FullTree A p) A (S n) -> APL.T (FullTree A (S p)) A n :=
+  match n with
+  | 0 =>    fun l =>
+              let '(t,(a,s)) := l in
+              Node t a s
+  | S n' => fun l =>
+              let '(t,((a,s),l)) := l in
+              ( Node t a s ,
+                PL.map (fun q => let '((a,t),(b,s)) := q in ( a , Node t b s ) ) l )
+  end
+.
+
+(** Alternative definition of [plus] which follows our recursion. *)
+Fixpoint plus (n p:nat) : nat :=
+  match n with
+  | 0 => p
+  | S n => plus n (S p)
+  end
+.
+
+Fixpoint balance_powerlist {A n p} : APL.T (FullTree A p) A n -> FullTree A (plus n p) :=
+  match n with
+  | 0 => fun t => t
+  | S n => fun l => balance_powerlist (pass l)
+  end
+.
+
+Definition singleton {A:Type} (x:A) : FullTree A 1 :=
+  Node Leaf₀ x Leaf₀
+.
+
+Definition balance {A:Type} (l:list A) : { n:nat & FullTree A n } :=
+  let '(existT n l') := APL.of_list Leaf₁ singleton (fun x=>x) l in
+  ⟨ _ , plus n 1 , balance_powerlist l' ⟩
+.
---
+++ src/full.ml
@@ -0,0 +1,192 @@
+(** The goal is to write a function [f:'a list -> 'a tree] which runs
+    in linear time, such that [f l] is the is full ({i i.e.} only the last
+    level may be incomplete) and that [l] contains the elements of [f l]
+    in infix order. In particular, if [l] is sorted, then [f l] is a
+    well-balance binary search tree.
+
+    The idea is to pair up values in the list, then pair up the values
+    in the paired-up list, and so on until there is a single
+    element. Since we are building a tree with internal nodes, though,
+    this requires a bit of tweaking: instead of pairing up, we have to
+    groupe three elements, the middle one being and ['a] and the other
+    two an ['a tree]. Also, this algorithm leads to a full tree only
+    when the length of the input list is of size [2^n-1] for some [n].
+
+    One way to implement this algorithm is to use a list of length
+    [2^h-1], with the invariant that every element at an even position
+    is a one-element tree. But I'll encode the invariants using
+    datatypes. *)
+type 'a tree =
+  | Node of 'a tree * 'a * 'a tree
+  | Leaf
+
+module NEList = struct
+
+  (** Non-empty lists *)
+  type 'a t = { hd:'a ; tl:'a list }
+
+  let cons' hd tl = { hd ; tl }
+  let cons a { hd ; tl } = cons' a (hd::tl)
+
+end
+
+module PowerList = struct
+
+  (** To enforce that a structure has [2^n-1] elements in ocaml's type
+      system, we can use non-uniform inductive datatypes. Using the
+      recursion that [2^(n+1)-1 = 2*(2^n-1)+1].
+
+      To read the ['a t] type, notice that there is always an element
+      available. It is the first (possibly unique) element of the
+      list. If there are other elements, then the next head is a pair
+      [(b,c)]: the second and third elements. If there are still more
+      elements, then the next head of head is a pair of pairs
+      [((d,e),(f,g))]: [d] is the fourth element, [e] the fifth, [f]
+      the sixth, and [g] the seventh. And so on. *)
+  type 'a t =
+    | One of 'a
+    | TwicePlusOne of 'a * ('a*'a) t
+
+  (** [map f l] is the functorial action of [f] on the powerlist [l]. *)
+  let rec map : 'a 'b. ('a->'b) -> 'a t -> 'b t = fun f l ->
+    match l with
+    | One x -> One (f x)
+    | TwicePlusOne (x,l) ->
+        let f' (x,y) = f x , f y in
+        TwicePlusOne ( f x , map f' l)
+
+  (** [pair_up l] takes the non-empty list [l] and returns, if [l] has
+      even length [Even a b,l'] where [(a,b)::l'] has consecutive
+      elements of [l] paired up, otherwise [Odd a,l'] where [l'] is
+      the (possibly empty) list of consecutive elements of [l.tl]
+      paired up. *)
+  type 'a eo =
+    | Odd of 'a
+    | Even of ('a*'a)
+  let rec pair_up = function
+    | { NEList.hd ; tl=[] } -> Odd hd , []
+    | { NEList.hd ; tl= b::l } ->
+        begin match pair_up (NEList.cons' b l) with
+        | Even bc,l' -> Odd hd , bc::l'
+        | Odd b,l' -> Even (hd,b) , l'
+        end
+
+  (** [pair_up_with_default d a l] returns the same result as [pair_up
+      a l] except that if there is an odd [a], then [d a] is added to
+      the front of the list, otherwise [a,b] is added to the front of
+      the list. Returning a non-empty list. *)
+  let pair_up_with_default d l =
+    match pair_up l with
+    | Odd a , l -> NEList.cons' (d a) l
+    | Even ab , l -> NEList.cons' ab l
+
+
+  (** We can construct an ['a t] from a non-empty ['a list] of any size, but it
+      requires padding. The idea is to insert a default value between
+      each element of the original list, until there are sufficiently
+      many to complete the tree. One should think of the original [d]
+      to be of the form [fun a -> (x,a)]. *)
+  let rec of_ne_list_with_default : 'a. ('a->'a*'a) -> 'a NEList.t -> 'a t = fun d l ->
+    match l with
+    | { NEList.hd ; tl=[] } -> One hd
+    | { NEList.hd ; tl=b::l } ->
+        let d' (x,y) = (d x,d y) in
+        let l' = NEList.cons' b l in
+        TwicePlusOne ( hd , of_ne_list_with_default d' (pair_up_with_default d l') )
+
+  (** We construct an ['a t] from an ['a list] using
+      {!of_ne_list_with_default} for the non-empty case, and a default
+      value for the empty case. *)
+  let of_list_with_default d = function
+    | [] -> One d
+    | a::l -> of_ne_list_with_default (fun x -> d,x) (NEList.cons' a l)
+
+  (** In this variant of {!of_ne_list_with_default}, elements of the
+      list are casted before being introduced in the
+      tree. [of_ne_list_with_cast d f b l] returns the power list starting
+      with [b] and continued with the elements of [List.map f l] with
+      default elements introduced by [d] when needed. *)
+  let rec of_ne_list_with_cast : 'a 'b. ('a->'b*'b) -> ('a->'b) -> 'b -> 'a list -> 'b t =
+    fun d f b l ->
+    let cast = function
+      | Odd a -> d a
+      | Even (a,b) -> (f a, f b)
+    in
+    match l with
+    | [] -> One b
+    | a::l ->
+        let d' (x,y) = (d x , d y) in
+        let f' (x,y) = (f x , f y) in
+        let (a',l') = pair_up (NEList.cons' a l) in
+        TwicePlusOne ( b , of_ne_list_with_cast d' f' (cast a') l' )
+
+end
+
+(** The [PowerList] module shows how to encode the length invariant,
+    and how to complete a list with a default value spliced between the
+    appropriate initial segment. However, for our algorithm, we also need
+    to encode the invariant that even positions have a different type than
+    odd position. *)
+
+module AlternatingPowerList = struct
+
+  (** In an [('odd,'even) t], the first head has type ['odd], the
+      second head is the pair of the the second and third element,
+      hence has type [('even,'odd)]. The third head has the elements
+      number four to seven: its type is
+      [(('even,'odd),('even,'odd))]. Indeed, once the first element
+      has been selected, even and odd elements pair up, and there is
+      no distinction between odd and even position pairs any
+      more. So we reuse the type {!PowerList.t}. *)
+  type ('odd,'even) t =
+    | One of 'odd
+    | TwicePlusOne of 'odd * ('even*'odd) PowerList.t
+
+
+  (** We build an [('odd,'even) t] from an ['a list] using a default
+      ['odd] value for padding, a function [f:'a -> 'odd] for odd
+      positions, and [g:'a -> 'even] for even positions. *)
+  let of_list d f g = function
+    | [] -> One d
+    | [a] -> One (f a)
+    | a::b::l ->
+        let d' x = g x , d in
+        let fg (x,y) = g x , f y in
+        let dd (x,y) = d' x , d' y in
+        let (b',l') = PowerList.pair_up (NEList.cons' b l) in
+        let b'' =
+          match b' with
+          | PowerList.Odd b -> d' b
+          | PowerList.Even bc -> fg bc
+        in
+        TwicePlusOne ( f a , PowerList.of_ne_list_with_cast dd fg b'' l')
+
+end
+
+module PL = PowerList
+module APL = AlternatingPowerList
+
+
+(** [pass join t l] takes a non-empty alternating power list
+    [APL.TwicePlusOne(t,l)], and groups the consecutive triplets using
+    to [join]: the three first elements (of respective types ['o],
+    ['e] and ['o]) are joined, then for each group of four elements
+    (of type [('e,'o),('e,'o)]) the first one is kept as such, and the
+    three others can be joined. *)
+let pass : 'o 'e. ('o->'e->'o->'o) -> 'o -> ('e*'o) PL.t -> ('o,'e) APL.t =
+  fun join t l ->
+    match l with
+    | PL.One (a,s) -> APL.One (join t a s)
+    | PL.TwicePlusOne((a,s),l) ->
+        APL.TwicePlusOne ( join t a s ,
+                           PL.map (fun ((a,t),(b,s)) -> a , join t b s ) l )
+
+let rec balance_powerlist : 'e. ('e tree,'e) APL.t -> 'e tree = function
+  | APL.One t -> t
+  | APL.TwicePlusOne (t,l) ->
+      let join t a s = Node(t,a,s) in
+      balance_powerlist (pass join t l)
+
+let singleton x = Node(Leaf,x,Leaf)
+let balance l =
+  balance_powerlist (APL.of_list Leaf (fun o->singleton o) (fun e->e) l)
