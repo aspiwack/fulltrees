23e8ec9adc143cc4b685f4e0d7ca5c5cd178660f
Guyslain Naves <guyslain.naves@lif.univ-mrs.fr>
2014-01-23 15:02:23 +0100

    Guyslain: add naive code for balancing a list

---
+++ src/naive.ml
@@ -0,0 +1,33 @@
+type 'a tree =
+| Leaf
+| Node of 'a tree * 'a * 'a tree
+
+type 'a tree_or_elt =
+| Elt of 'a
+| Tree of 'a tree
+
+let join left node right = Tree (Node (left, node, right))
+
+let rec pass list = match  list with
+  | Tree left :: Elt root :: Tree right :: Elt e :: others -> join left root right :: Elt e :: pass others
+  | [Tree left; Elt root; Tree right] -> [join left root right]
+  | _ -> assert false
+
+let rec balance list = match list with
+  | [] -> Leaf
+  | [Tree t] -> t
+  | list -> balance (pass list)
+
+let complete list =
+  let n = List.length list in
+  let rec pow2 i = if i <= n then pow2 (2*i) else i in 
+  let missing = (pow2 1) - n - 1 in
+  let rec pad missing = function
+    | head::tail when missing <> 0 -> Tree Leaf :: Elt head :: pad (missing - 1) tail
+    | one::two::others -> join Leaf one Leaf :: Elt two :: pad 0 others
+    | [single] -> [join Leaf single Leaf]
+    | [] -> []
+  in
+  pad missing list
+
+let rebuild list = balance (complete  list)
